e33b0e18cf867d2f98950b59c8faa1995557238d
jlathon61 <81202944+jlathon61@users.noreply.github.com>
2021-05-24 21:23:10 -0400

    initial commit

---
+++ src/main/java/com/tts/techTalentTwitter/repository/TagRepository.java
@@ -0,0 +1,13 @@
+package com.tts.techTalentTwitter.repository;
+
+import com.tts.techTalentTwitter.model.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long>  {
+
+    Tag findByPhrase(String phrase);
+
+}
+
---
+++ src/main/java/com/tts/techTalentTwitter/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tts.techTalentTwitter.repository;
+
+import com.tts.techTalentTwitter.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
